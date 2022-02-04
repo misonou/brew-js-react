b0b39106755bfdd817cde987e74b921903c66493
Takeru <takeru.moe@gmail.com>
2022-02-04 15:12:08 +0800

    fix(mixin): focus and loading state mixin may not return correct class names

--- src/mixins/FocusStateMixin.js
+++ src/mixins/FocusStateMixin.js
@@ -1,24 +1,29 @@
 import { definePrototype } from "../include/zeta-dom/util.js";
 import { setClass } from "../include/zeta-dom/domUtil.js";
 import dom from "../include/zeta-dom/dom.js";
-import ClassNameMixin from "./ClassNameMixin.js";
+import StatefulMixin from "./StatefulMixin.js";
 
-const FocusStateMixinSuper = ClassNameMixin.prototype;
+const FocusStateMixinSuper = StatefulMixin.prototype;
 
 export default function FocusStateMixin() {
-    ClassNameMixin.call(this, ['focused']);
+    StatefulMixin.call(this);
 }
 
-definePrototype(FocusStateMixin, ClassNameMixin, {
+definePrototype(FocusStateMixin, StatefulMixin, {
     initElement: function (element, state) {
         FocusStateMixinSuper.initElement.call(this, element, state);
         dom.on(element, {
             focusin: function () {
+                state.focused = true;
                 setClass(element, 'focused', true);
             },
             focusout: function () {
+                state.focused = false;
                 setClass(element, 'focused', false);
             }
         });
+    },
+    getClassNames: function () {
+        return [{ focused: !!this.state.focused }];
     }
 });
--- src/mixins/LoadingStateMixin.js
+++ src/mixins/LoadingStateMixin.js
@@ -1,27 +1,33 @@
 import { definePrototype } from "../include/zeta-dom/util.js";
 import { setClass } from "../include/zeta-dom/domUtil.js";
 import dom from "../include/zeta-dom/dom.js";
-import ClassNameMixin from "./ClassNameMixin.js";
+import StatefulMixin from "./StatefulMixin.js";
 
-const LoadingStateMixinSuper = ClassNameMixin.prototype;
+const LoadingStateMixinSuper = StatefulMixin.prototype;
 
 export default function LoadingStateMixin() {
-    ClassNameMixin.call(this, ['loading']);
+    StatefulMixin.call(this);
 }
 
-definePrototype(LoadingStateMixin, ClassNameMixin, {
+definePrototype(LoadingStateMixin, StatefulMixin, {
     initElement: function (element, state) {
         LoadingStateMixinSuper.initElement.call(this, element, state);
         dom.on(element, {
             asyncStart: function () {
+                state.loading = true;
                 setClass(element, 'loading', true);
             },
             asyncEnd: function () {
+                state.loading = false;
                 setClass(element, 'loading', false);
             },
             cancelled: function () {
+                state.loading = false;
                 setClass(element, 'loading', false);
             }
         });
+    },
+    getClassNames: function () {
+        return [{ loading: !!this.state.loading }];
     }
 });
