86c13fc2ff5d73f48c2b9b821ca435c107dd9870
Takeru <takeru.moe@gmail.com>
2022-09-19 15:28:38 +0800

    fix(mixin): subscribe async needed for loading mixin

--- src/mixins/LoadingStateMixin.js
+++ src/mixins/LoadingStateMixin.js
@@ -1,5 +1,6 @@
 import { definePrototype } from "../include/zeta-dom/util.js";
 import { setClass } from "../include/zeta-dom/domUtil.js";
+import { lock } from "../include/zeta-dom/domLock.js";
 import dom from "../include/zeta-dom/dom.js";
 import StatefulMixin from "./StatefulMixin.js";
 
@@ -12,6 +13,7 @@ export default function LoadingStateMixin() {
 definePrototype(LoadingStateMixin, StatefulMixin, {
     initElement: function (element, state) {
         LoadingStateMixinSuper.initElement.call(this, element, state);
+        lock(element);
         dom.on(element, {
             asyncStart: function () {
                 state.loading = true;
