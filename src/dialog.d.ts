40c1e450201f300cad2dfd882fca3ec822c2177c
Takeru <takeru.moe@gmail.com>
2024-09-27 22:05:28 +0800

    update(dialog): update function signature

--- src/dialog.d.ts
+++ src/dialog.d.ts
@@ -1,6 +1,18 @@
 import { ErrorHandler } from "zeta-dom-react";
 
-export type DialogCloseCallback<T> = (value?: T) => Promise<void>;
+type PartialRequired<T, K extends keyof T> = Omit<T, K> & { [P in K]-?: T[P] }
+type VoidOrOptional<T> = [Exclude<T, undefined | void>] extends [never] ? void : Exclude<T, undefined | void>;
+
+export type DialogCloseCallback<T> = Zeta.IsAnyOrUnknown<T> extends true ?
+    (value?: any) => Promise<void> :
+    (value: T extends undefined ? void | undefined : T) => Promise<void>;
+export type DialogResult<T> = T extends void ? undefined : Zeta.IsAnyOrUnknown<T> extends true ? any : T | undefined;
+export type DialogOptionsStrict<T, V> = PartialRequired<DialogOptions<T, V>, 'onCommit'>;
+
+/** @deprecated */
+export type DialogBaseProps<T, V = T> = Omit<DialogOptions<T, V | undefined>, 'onRender' | 'wrapper'>;
+/** @deprecated */
+export type DialogRenderComponentProps<T, V = T> = DialogOptions<T, V | undefined> & DialogContext<V | undefined>;
 
 export interface DialogState<T> {
     /**
@@ -10,19 +22,19 @@ export interface DialogState<T> {
     /**
      * Opens the dialog.
      *
-     * It will receive result from {@link DialogState.close} or {@link DialogRenderComponentProps.closeDialog} when the dialog is properly closed.
+     * It will receive result from {@link DialogState.close} or {@link DialogContext.closeDialog} when the dialog is properly closed.
      * Otherwise in cases such as being closed due to lost of focus, result value will always be `undefined`.
      */
-    open: () => Promise<T | undefined>;
+    open(): Promise<DialogResult<T>>;
     /**
      * Closes the dialog, with optional result value.
      *
-     * This method is different from {@link DialogRenderComponentProps.closeDialog} that {@link DialogBaseProps.onCommit} will not be called.
+     * This method is similar to {@link DialogContext.dismissDialog} that {@link DialogOptions.onCommit} will not be called.
      */
-    close: (value?: T) => Promise<void>;
+    close(value?: T): Promise<void>;
 }
 
-export interface DialogBaseProps<T, V = T> {
+export interface DialogOptions<T, V = T | undefined> {
     /**
      * Specifies dialog title.
      * This property is intended to be handled by {@link DialogOptions.onRender} or {@link DialogOptions.wrapper}.
@@ -60,13 +72,13 @@ export interface DialogBaseProps<T, V = T> {
     /**
      * Callback to perform asynchronous action when user confirms the dialog.
      *
-     * The callback will receive value sent from {@link DialogRenderComponentProps.closeDialog},
+     * The callback will receive value sent from {@link DialogContext.commitDialog} or {@link DialogContext.closeDialog},
      * and will send resolved value to promise returned by {@link DialogState.open}.
      *
      * The dialog will be held open until the returned promise resolves.
      * When the promise rejects, the dialog will remain open.
      */
-    onCommit?: (value: V | undefined, context: Zeta.RunAsyncContext) => T | Promise<T> | void;
+    onCommit?: (value: V, context: Zeta.RunAsyncContext) => T | Promise<T> | undefined | void;
     /**
      * Callback to be invoked when dialog has opened.
      */
@@ -75,9 +87,18 @@ export interface DialogBaseProps<T, V = T> {
      * Callback to be invoked when dialog has closed, after outro animation and before React DOM is unmounted.
      */
     onClose?: (root: HTMLElement) => void;
+    /**
+     * A callback that render dialog contents.
+     */
+    onRender: React.VFC<DialogContext<V> & this>;
+    /**
+     * Specifies wrapper component that envelops content from {@link DialogOptions.onRender},
+     * which is useful for reusable layout for dialogs.
+     */
+    wrapper?: React.FC<DialogContext<V> & this & React.PropsWithChildren<{}>>;
 }
 
-export interface DialogRenderComponentProps<T, V = T> extends DialogBaseProps<T, V> {
+export interface DialogContext<V> {
     /**
      * Gets an {@link ErrorHandler} object that catches errors emitted from rendered content.
      */
@@ -85,35 +106,23 @@ export interface DialogRenderComponentProps<T, V = T> extends DialogBaseProps<T,
     /**
      * Commits the dialog with result value.
      *
-     * When {@link DialogBaseProps.onCommit} is specified, it is invoked with the value passed in.
+     * When {@link DialogOptions.onCommit} is specified, it is invoked with the value passed in.
      * Dialog will be closed when async operation is completed, or remain open if the operation failed.
      *
-     * @deprecated Use {@link DialogRenderComponentProps.commitDialog} or {@link DialogRenderComponentProps.dismissDialog} for appropriate cases.
+     * @deprecated Alias of {@link DialogContext.commitDialog}.
      */
-    closeDialog: (value?: V) => Promise<void>;
+    closeDialog: DialogCloseCallback<V>;
     /**
      * Commits the dialog with result value.
      *
-     * When {@link DialogBaseProps.onCommit} is specified, it is invoked with the value passed in.
+     * When {@link DialogOptions.onCommit} is specified, it is invoked with the value passed in.
      * Dialog will be closed when async operation is completed, or remain open if the operation failed.
      */
-    commitDialog: (value?: V) => Promise<void>;
+    commitDialog: DialogCloseCallback<V>;
     /**
-     * Closes the dialog without invoking {@link DialogBaseProps.onCommit}.
+     * Closes the dialog without invoking {@link DialogOptions.onCommit}.
      */
-    dismissDialog: () => Promise<void>;
-}
-
-export interface DialogOptions<T, V = T> extends DialogBaseProps<T, V> {
-    /**
-     * A callback that render dialog contents.
-     */
-    onRender: React.FC<DialogRenderComponentProps<T, V> & this>;
-    /**
-     * Specifies wrapper component that envelops content from {@link DialogOptions.onRender},
-     * which is useful for reusable layout for dialogs.
-     */
-    wrapper?: React.FC<React.PropsWithChildren<DialogRenderComponentProps<T, V> & this>>;
+    dismissDialog: DialogCloseCallback<void>;
 }
 
 export interface DialogProps<T, V = T> extends React.PropsWithChildren<DialogBaseProps<T, V>> {
@@ -127,7 +136,15 @@ export interface DialogProps<T, V = T> extends React.PropsWithChildren<DialogBas
  * Creates a controller to render dialog.
  * @param props A dictionary containing options.
  */
-export function createDialog<T = any, V = T>(props: DialogOptions<T, V>): DialogState<T>;
+export function createDialog<T, V>(props: DialogOptionsStrict<T, V>): DialogState<VoidOrOptional<T>>;
+
+export function createDialog<T, V>(props: DialogOptions<T, V>): DialogState<VoidOrOptional<T | V>>;
+
+export function createDialog<T, V = T>(props: DialogOptions<T, V | undefined>): DialogState<VoidOrOptional<T | V>>;
+
+export function createDialog<P extends DialogOptionsStrict<any, any>>(props: P): DialogState<VoidOrOptional<P extends DialogOptionsStrict<infer T, any> ? T : unknown>>;
+
+export function createDialog<P extends DialogOptions<any, any>>(props: P): DialogState<VoidOrOptional<P extends DialogOptions<infer T, infer V> ? T | V : unknown>>;
 
 /**
  * Renders a dialog declaratively.
--- tests/types.ts
+++ tests/types.ts
@@ -1,11 +1,313 @@
 import { expectTypeOf } from "expect-type";
+import { createDialog, DialogOptions, DialogOptionsStrict, DialogRenderComponentProps, DialogState } from "src/dialog";
 import { navigateTo, registerView, ViewComponent, ViewProps } from "src/view";
 
 declare const ReactContent: JSX.Element;
 declare const _: unknown;
 
+type Func<T, R> = (value: T) => R;
+type FuncOp<T, R> = (value?: T) => R;
 type A = { a: number };
 type B = { b: number };
+
+// -------------------------------------
+// dialog.d.ts
+
+declare const onRender: React.VFC<{}>;
+declare const onCommit: Func<any, any>;
+
+// infer return type by explicit T (with onCommit)
+expectTypeOf(createDialog<void>({ onRender, onCommit })).toEqualTypeOf<DialogState<void>>();
+expectTypeOf(createDialog<undefined>({ onRender, onCommit })).toEqualTypeOf<DialogState<void>>();
+expectTypeOf(createDialog<boolean>({ onRender, onCommit })).toEqualTypeOf<DialogState<boolean>>();
+expectTypeOf(createDialog<boolean | undefined>({ onRender, onCommit })).toEqualTypeOf<DialogState<boolean>>();
+
+// infer return type by explicit T (without onCommit)
+expectTypeOf(createDialog<void>({ onRender })).toEqualTypeOf<DialogState<void>>();
+expectTypeOf(createDialog<undefined>({ onRender })).toEqualTypeOf<DialogState<void>>();
+expectTypeOf(createDialog<boolean>({ onRender })).toEqualTypeOf<DialogState<boolean>>();
+expectTypeOf(createDialog<boolean | undefined>({ onRender })).toEqualTypeOf<DialogState<boolean>>();
+
+// infer return type by explicit T and V (with onCommit)
+expectTypeOf(createDialog<boolean, string>({ onRender, onCommit })).toEqualTypeOf<DialogState<boolean>>();
+expectTypeOf(createDialog<boolean, void>({ onRender, onCommit })).toEqualTypeOf<DialogState<boolean>>();
+
+// infer return type by explicit T and V (without onCommit)
+expectTypeOf(createDialog<boolean, string>({ onRender })).toEqualTypeOf<DialogState<boolean | string>>();
+expectTypeOf(createDialog<boolean, void>({ onRender })).toEqualTypeOf<DialogState<boolean>>();
+
+// infer return type by onCommit
+expectTypeOf(createDialog({ onRender, onCommit: <Func<boolean, void>>_ })).toEqualTypeOf<DialogState<void>>();
+expectTypeOf(createDialog({ onRender, onCommit: <Func<boolean, string>>_ })).toEqualTypeOf<DialogState<string>>();
+expectTypeOf(createDialog({ onRender, onCommit: <FuncOp<boolean, void>>_ })).toEqualTypeOf<DialogState<void>>();
+expectTypeOf(createDialog({ onRender, onCommit: <FuncOp<boolean, string>>_ })).toEqualTypeOf<DialogState<string>>();
+
+// infer return type by explicit optional onCommit
+expectTypeOf(createDialog({ onRender, onCommit: <Func<boolean, void> | undefined>_ })).toEqualTypeOf<DialogState<boolean>>();
+expectTypeOf(createDialog({ onRender, onCommit: <Func<boolean, string> | undefined>_ })).toEqualTypeOf<DialogState<boolean | string>>();
+expectTypeOf(createDialog({ onRender, onCommit: <FuncOp<boolean, void> | undefined>_ })).toEqualTypeOf<DialogState<boolean>>();
+expectTypeOf(createDialog({ onRender, onCommit: <FuncOp<boolean, string> | undefined>_ })).toEqualTypeOf<DialogState<boolean | string>>();
+
+// infer return type by DialogOptions
+expectTypeOf(createDialog(<DialogOptions<boolean>>_)).toEqualTypeOf<DialogState<boolean>>();
+expectTypeOf(createDialog(<DialogOptions<boolean, void>>_)).toEqualTypeOf<DialogState<boolean>>();
+expectTypeOf(createDialog(<DialogOptions<boolean, string>>_)).toEqualTypeOf<DialogState<boolean | string>>();
+expectTypeOf(createDialog(<DialogOptionsStrict<boolean, void>>_)).toEqualTypeOf<DialogState<boolean>>();
+expectTypeOf(createDialog(<DialogOptionsStrict<boolean, string>>_)).toEqualTypeOf<DialogState<boolean>>();
+
+// infer return type by DialogRenderComponentProps
+expectTypeOf(createDialog<boolean>({ onRender: (_1: DialogRenderComponentProps<boolean>) => ReactContent })).toEqualTypeOf<DialogState<boolean>>();
+expectTypeOf(createDialog<boolean, void>({ onRender: (_1: DialogRenderComponentProps<boolean, void>) => ReactContent })).toEqualTypeOf<DialogState<boolean>>();
+expectTypeOf(createDialog<boolean, string>({ onRender: (_1: DialogRenderComponentProps<boolean, string>) => ReactContent })).toEqualTypeOf<DialogState<boolean | string>>();
+expectTypeOf(createDialog<boolean>({ onCommit, onRender: (_1: DialogRenderComponentProps<boolean>) => ReactContent })).toEqualTypeOf<DialogState<boolean>>();
+expectTypeOf(createDialog<boolean, void>({ onCommit, onRender: (_1: DialogRenderComponentProps<boolean, void>) => ReactContent })).toEqualTypeOf<DialogState<boolean>>();
+expectTypeOf(createDialog<boolean, string>({ onCommit, onRender: (_1: DialogRenderComponentProps<boolean, string>) => ReactContent })).toEqualTypeOf<DialogState<boolean | string>>();
+
+// DialogState.open
+expectTypeOf(createDialog<void>({ onRender }).open()).toEqualTypeOf<Promise<undefined>>();
+expectTypeOf(createDialog<undefined>({ onRender }).open()).toEqualTypeOf<Promise<undefined>>();
+expectTypeOf(createDialog<boolean>({ onRender }).open()).toEqualTypeOf<Promise<boolean | undefined>>();
+expectTypeOf(createDialog<unknown>({ onRender }).open()).toEqualTypeOf<Promise<any>>();
+
+// DialogState.close
+expectTypeOf(createDialog<void>({ onRender }).close()).toEqualTypeOf<Promise<void>>();
+expectTypeOf(createDialog<boolean>({ onRender }).close()).toEqualTypeOf<Promise<void>>();
+expectTypeOf(createDialog<boolean>({ onRender }).close(undefined)).toEqualTypeOf<Promise<void>>();
+expectTypeOf(createDialog<boolean>({ onRender }).close(true)).toEqualTypeOf<Promise<void>>();
+// @ts-expect-error: incorrect argument type
+createDialog<boolean>({ onRender }).close("");
+
+// DialogOptions
+// @ts-check: backward compatibility - single type argument should imply optional commit argument
+createDialog<boolean>({ ...<DialogOptions<boolean>>_ });
+
+// infer argument type of onCommit from explicit T and V
+// @ts-check: backward compatibility - single type argument should imply optional commit argument
+createDialog<boolean>({
+    onRender,
+    onCommit(value) {
+        expectTypeOf(value).toEqualTypeOf<boolean | undefined>();
+    }
+});
+createDialog<void, boolean>({
+    onRender,
+    onCommit(value) {
+        expectTypeOf(value).toEqualTypeOf<boolean>();
+    }
+});
+createDialog<boolean, string>({
+    onRender,
+    onCommit(value) {
+        expectTypeOf(value).toEqualTypeOf<string>();
+        return true;
+    }
+});
+createDialog<boolean, void>({
+    onRender,
+    onCommit(value) {
+        expectTypeOf(value).toEqualTypeOf<void>();
+        return true;
+    }
+});
+createDialog<boolean, void>({
+    onRender,
+    // @ts-expect-error: incorrect type
+    onCommit(value) {
+        expectTypeOf(value).toEqualTypeOf<void>();
+        return "true";
+    }
+});
+
+// infer argument type of onRender (required onCommit)
+createDialog({
+    onCommit: <Func<boolean, void>>_,
+    onRender(props) {
+        props.commitDialog(true);
+        // @ts-expect-error: expect parameter
+        props.commitDialog();
+        // @ts-expect-error: incorrect type
+        props.commitDialog("true");
+        return ReactContent;
+    }
+});
+createDialog({
+    onCommit: <FuncOp<boolean, void>>_,
+    onRender(props) {
+        props.commitDialog(true);
+        props.commitDialog();
+        // @ts-expect-error: incorrect type
+        props.commitDialog("true");
+        return ReactContent;
+    }
+});
+createDialog({
+    onCommit: <Func<void, void>>_,
+    onRender(props) {
+        props.commitDialog();
+        // @ts-expect-error: incorrect type
+        props.commitDialog(true);
+        return ReactContent;
+    }
+});
+// @ts-check: commit argument can be any type if onCommit does not take arguments
+createDialog({
+    onCommit: <(() => true)>_,
+    onRender(props) {
+        props.commitDialog();
+        props.commitDialog(true);
+        props.commitDialog("true");
+        return ReactContent;
+    }
+});
+// @ts-check: backward compatibility - single type argument should imply optional commit argument
+createDialog<boolean>({
+    onCommit,
+    onRender(props) {
+        props.commitDialog(true);
+        props.commitDialog();
+        // @ts-expect-error: incorrect type
+        props.commitDialog("true");
+        return ReactContent;
+    }
+});
+createDialog<boolean, string>({
+    onCommit,
+    onRender(props) {
+        props.commitDialog("");
+        // @ts-expect-error: incorrect type
+        props.commitDialog(true);
+        return ReactContent;
+    }
+});
+createDialog<boolean, void>({
+    onCommit,
+    onRender(props) {
+        props.commitDialog(undefined);
+        props.commitDialog();
+        // @ts-expect-error: incorrect type
+        props.commitDialog(true);
+        return ReactContent;
+    }
+});
+createDialog<boolean, undefined>({
+    onCommit,
+    onRender(props) {
+        props.commitDialog(undefined);
+        props.commitDialog();
+        // @ts-expect-error: incorrect type
+        props.commitDialog(true);
+        return ReactContent;
+    }
+});
+
+// infer argument type of onRender (optional onCommit)
+createDialog({
+    onCommit: <Func<boolean, void> | undefined>_,
+    onRender(props) {
+        props.commitDialog(true);
+        // @ts-expect-error: expect parameter
+        props.commitDialog();
+        // @ts-expect-error: incorrect type
+        props.commitDialog("true");
+        return ReactContent;
+    }
+});
+createDialog({
+    onCommit: <FuncOp<boolean, void> | undefined>_,
+    onRender(props) {
+        props.commitDialog(true);
+        props.commitDialog();
+        // @ts-expect-error: incorrect type
+        props.commitDialog("true");
+        return ReactContent;
+    }
+});
+createDialog({
+    onCommit: <Func<void, void> | undefined>_,
+    onRender(props) {
+        props.commitDialog();
+        // @ts-expect-error: incorrect type
+        props.commitDialog(true);
+        return ReactContent;
+    }
+});
+// @ts-check: commit argument can be any type if onCommit does not take arguments
+createDialog({
+    onCommit: <(() => true) | undefined>_,
+    onRender(props) {
+        props.commitDialog();
+        props.commitDialog(true);
+        props.commitDialog("true");
+        return ReactContent;
+    }
+});
+// @ts-check: commit argument can be any type if it's type cannot be inferred
+createDialog({
+    onRender(props) {
+        props.commitDialog();
+        props.commitDialog(true);
+        props.commitDialog("true");
+        return ReactContent;
+    }
+});
+// @ts-check: backward compatibility - single type argument should imply optional commit argument
+createDialog<boolean>({
+    onRender(props) {
+        props.commitDialog(true);
+        props.commitDialog();
+        // @ts-expect-error: incorrect type
+        props.commitDialog("true");
+        return ReactContent;
+    }
+});
+createDialog<boolean, string>({
+    onRender(props) {
+        props.commitDialog("");
+        // @ts-expect-error: incorrect type
+        props.commitDialog(true);
+        return ReactContent;
+    }
+});
+createDialog<boolean, void>({
+    onRender(props) {
+        props.commitDialog(undefined);
+        props.commitDialog();
+        // @ts-expect-error: incorrect type
+        props.commitDialog(true);
+        return ReactContent;
+    }
+});
+createDialog<boolean, undefined>({
+    onRender(props) {
+        props.commitDialog(undefined);
+        props.commitDialog();
+        // @ts-expect-error: incorrect type
+        props.commitDialog(true);
+        return ReactContent;
+    }
+});
+
+// infer child property for wrapper
+createDialog({
+    onRender,
+    wrapper(props) {
+        expectTypeOf(props).toMatchTypeOf<React.PropsWithChildren<{}>>();
+        return ReactContent;
+    }
+});
+
+// infer properties from extended type
+createDialog(<DialogOptions<boolean> & { extraProp: boolean }>{
+    extraProp: true,
+    wrapper(props) {
+        expectTypeOf(props).toMatchTypeOf<{ extraProp: Boolean }>();
+        return ReactContent;
+    },
+    onRender(props) {
+        expectTypeOf(props).toMatchTypeOf<{ extraProp: Boolean }>();
+        return ReactContent;
+    }
+});
 
 // -------------------------------------
 // view.d.ts
